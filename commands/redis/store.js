8c12183ac5a2661c41f143785c86b0aab06dee3c
Yuan Chuan <spirit23@163.com>
2013-04-14 22:21:01 +0800

    cleanup

--- commands/redis/store.js
+++ commands/redis/store.js
@@ -1,29 +1,29 @@
 
-var Store = module.exports = function Store() {
+var store = module.exports = function store() {
   this.memo = Object.create(null);
 }
 
-Store.prototype.exists = function(key) {
+store.prototype.exists = function(key) {
   return {}.hasOwnProperty.call(this.memo, key);
 }  
 
-Store.prototype.get = function(key) {
+store.prototype.get = function(key) {
   return this.memo[key];
 }
  
-Store.prototype.set = function(key, name) {
+store.prototype.set = function(key, name) {
   this.memo[key] = name;
 }
 
-Store.prototype.del = function(key) {
+store.prototype.del = function(key) {
   delete this.memo[key];
 }
 
-Store.prototype.keys = function() {
+store.prototype.keys = function() {
   return Object.keys(this.memo);
 }
 
-Store.prototype.delAll = function() {
+store.prototype.delAll = function() {
   this.memo = Object.create(null);
 }
 
