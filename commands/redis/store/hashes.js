d83dd04386c481b299c54f6ce7a41632541e4fa6
Yuan Chuan <spirit23@163.com>
2013-04-16 10:50:12 +0800

    update hash for adapting new code structure

--- commands/redis/store/hashes.js
+++ commands/redis/store/hashes.js
@@ -1,5 +1,40 @@
 
-module.exports = function() {
-  
+var hashes = module.exports = function() {
+  this.memo = Object.create(null);  
+}
+
+hashes.prototype.exists = function(hkey, field) {
+  return (
+    {}.hasOwnProperty.call(this.memo, hkey) &&   
+    {}.hasOwnProperty.call(this.memo[hkey], field)
+  );
 }
  
+hashes.prototype.set = function(hkey, field, val) {
+  if (this.memo[hkey] === undefined) {
+    this.memo[hkey] = Object.create(null);
+  }
+  if (typeof val !== 'string') {
+    val = JSON.stringify(val);
+  }
+  this.memo[hkey][field] = val;
+}
+
+hashes.prototype.get = function(hkey, field) {
+  if (this.memo[hkey] !== undefined) {
+    return this.memo[hkey][field];
+  }
+}
+
+hashes.prototype.del = function(hkey, field) {
+  if (this.memo[hkey] !== undefined) {
+    delete this.memo[hkey][field];
+  }
+}
+
+hashes.prototype.fields = function(hkey) {
+  if (this.memo[hkey] !== undefined) {
+    return Object.keys(this.memo[hkey]);
+  }
+}
+
