6201363038e2cc499829e224a6810c8cfaf2505b
yuanchuan <yuanchuan23@gmail.com>
2013-04-25 22:34:33 +0800

    ping github

---
+++ commands/sets.js
@@ -0,0 +1,58 @@
+var Redis = module.exports = require('./redis');
+var R = Redis.prototype;
+
+R.sadd = function(skey, val) {
+
+}
+
+R.spop = function() {
+
+}
+
+R.srem = function() {
+
+}
+
+R.scard = function() {
+
+}
+
+R.sismember = function() {
+
+}
+
+R.smembers = function() {
+
+}
+
+R.srandmember = function() {
+
+}
+
+R.smove = function(){
+
+}
+
+R.sdiff = function() {
+
+}
+
+R.sdiffstore = function() {
+
+}
+
+R.sinter = function() {
+
+}
+
+R.sinterstore = function() {
+
+}
+
+R.sunion = function() {
+
+}
+
+R.sunionstore = function() {
+
+}
