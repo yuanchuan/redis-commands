ef6e47aed3113ad05f06dcf467c1e4e2bb0a3023
yuanchuan <yuanchuan23@gmail.com>
2013-04-26 15:21:22 +0800

    add basic sets structure

--- commands/redis/store/sets.js
+++ commands/redis/store/sets.js
@@ -1,5 +1,38 @@
 
-module.exports = function() {
+var sets = module.exports = function() {
+  this.memo = Object.create(null);  
+}
 
+sets.prototype.ismember = function(skey, member) {
+  return (
+    {}.hasOwnProperty.call(this.memo, skey) &&   
+    {}.hasOwnProperty.call(this.memo[skey], member)
+  );
+}
+ 
+sets.prototype.add = function(skey, member) {
+  if (this.memo[skey] === undefined) {
+    this.memo[skey] = Object.create(null);
+  }
+  this.memo[skey][member] = 1;
+}
+
+sets.prototype.del = function(skey, member) {
+  if (member === undefined) {
+    delete this.memo[skey];
+  }
+  if (this.memo[skey] !== undefined) {
+    delete this.memo[skey][member];
+  }
+}
+
+sets.prototype.delAll = function() {
+  this.memo = Object.create(null);  
+}
+
+sets.prototype.members = function(skey) {
+  if (this.memo[skey] !== undefined) {
+    return Object.keys(this.memo[skey]);
+  }
 }
 
